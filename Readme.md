b482b2548f16ff628f53a6b0340ae921640e89fa
pratyushabrs <pratyusha705@gmail.com>
2024-01-09 13:32:41 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Virtual Keyboard
+
+###### Purpose:
+    Virtual keyboard using JS
+
+#### Dear Students, Check out the live app [here]().
---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+const keys = document.querySelectorAll(".keys");
+const textarea = document.querySelector("textarea");
+const deleteBtn =document.getElementById("delete");
+const shiftBtn = document.getElementById("shift");
+const spaceBtn= document.getElementById("space");
+//default theme
+let chathamus_blue="#1A4B84";
+//init char array
+let chars =[];
+//for each to get the key press
+keys.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        textarea.value+=btn.innerText;
+        chars =textarea.value.split("");
+});
+});
+//delete button event
+deleteBtn.addEventListener("click",()=>{
+    chars.pop();
+    textarea.value = chars.join("");
+});
+//space bar event
+spaceBtn.addEventListener("click",()=>{
+    chars.push('');
+    textarea.value =chars.join("");
+});
+//Shift Button Event
+shiftBtn.addEventListener("click",()=>{
+    keys.forEach((btn)=>{
+        btn.classList.toggle('upper');
+   
+    });
+});
+// Change Theme
+function setTheme(theme) {
+    document.documentElement.style.setProperty("--primary-color", theme);
+    localStorage.setItem("movie-theme", theme);
+  }
+  setTheme(localStorage.getItem("movie-theme") || chathams_blue);
